136823f65dfdb30a1c7a25a1f56613372f3a69ab
Maxim Zaytsev <zaytsev.max90@gmail.com>
2018-12-16 13:14:59 +0300

    добавляем шаблон bs4-clear

---
+++ local/templates/bs4-clear/header.php
@@ -0,0 +1,22 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Page\Asset;
+Loc::loadMessages(__FILE__);
+?>
+<!doctype html>
+<html lang="<?=LANGUAGE_ID?>">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <?Asset::getInstance()->addCss(SITE_TEMPLATE_PATH."/src/css/main.css");?>
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <?$APPLICATION->ShowHead()?>
+</head>
+<body>
+<?$APPLICATION->ShowPanel();?>
+<h1 id="pagetitle"><?=$APPLICATION->ShowTitle(false);?></h1>
---
+++ local/templates/bs4-clear/lang/ru/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxim
+ * Date: 12.12.2018
+ * Time: 22:29
+ */
