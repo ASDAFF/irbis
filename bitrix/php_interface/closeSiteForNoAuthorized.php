39761badc155d7ba4437584b3546d6c9c583006a
Max zaitsev <zaytsev.max90@gmail.com>
2019-01-15 21:19:50 +0300

    IR-31 Перенаправлять незарегистрированных пользователей

--- bitrix/php_interface/closeSiteForNoAuthorized.php
+++ bitrix/php_interface/closeSiteForNoAuthorized.php
@@ -6,10 +6,16 @@
 $closeSites = array ('s2');
 $current_link  = $APPLICATION->GetCurPage();
 $auth_link = '/auth/';
-if(in_array(SITE_ID, $closeSites) && $current_link != $auth_link && !CUser::IsAuthorized()) {
+$change_password_link = '/auth/change-password/';
+if(in_array(SITE_ID, $closeSites) && $current_link != $auth_link && $current_link != $change_password_link && !CUser::IsAuthorized()) {
     ob_start();
     $new_url = $auth_link;
     header('Location: '.$new_url);
     ob_end_flush();
     die();
 }
+
+/*
+
+ http://b2b.td-irbis.ru/auth/change-password/index.php?change_password=yes&lang=ru&USER_CHECKWORD=17c0d7a59c494ba9b198f4cef1335008&USER_LOGIN=zaytsev.max90%40gmail.com
+ */
