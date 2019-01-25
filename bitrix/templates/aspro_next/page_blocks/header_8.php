832e535a292dffbae26c1296937614be993c2f6e
Unknown <alexmelkov@mail.ru>
2019-01-25 13:50:56 +0300

    Изменение логики поведения кнопки авторизации

--- bitrix/templates/aspro_next/page_blocks/header_8.php
+++ bitrix/templates/aspro_next/page_blocks/header_8.php
@@ -1,6 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?>
 <?
-global $arTheme, $arRegion;
+global $arTheme, $arRegion, $USER;
 $arRegions = CNextRegionality::getRegions();
 if($arRegion)
 	$bPhone = ($arRegion['PHONES'] ? true : false);
@@ -100,19 +100,36 @@ $logoClass = ($arTheme['COLORED_LOGO']['VALUE'] !== 'Y' ? '' : ' colored');
 						<div class="pull-right">
 							<div class="wrap_icon inner-table-block">
 <!--								--><?//=CNext::showCabinetLink(true, false, '');?>
-                                <a rel="nofollow" title="Авторизация для партнеров" class="personal-link dark-color" href="http://b2b.td-irbis.ru/auth/" target="_blank"><i class="svg inline  svg-inline-cabinet" aria-hidden="true" title="Мой кабинет"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="17" viewBox="0 0 14 17">
-                                            <defs>
-                                                <style>
-                                                    .uscls-1 {
-                                                        fill: #222;
-                                                        fill-rule: evenodd;
-                                                    }
-                                                </style>
-                                            </defs>
-                                            <path class="uscls-1" d="M13.969,16a1,1,0,1,1-2,0H11.927C11.578,14.307,9.518,13,7,13s-4.575,1.3-4.924,3H2.031a1,1,0,0,1-2,0,0.983,0.983,0,0,1,.1-0.424C0.7,12.984,3.54,11,7,11S13.332,13,13.882,15.6a1.023,1.023,0,0,1,.038.158c0.014,0.082.048,0.159,0.058,0.243H13.969ZM7,10a5,5,0,1,1,5-5A5,5,0,0,1,7,10ZM7,2a3,3,0,1,0,3,3A3,3,0,0,0,7,2Z"></path>
-                                        </svg>
-                                    </i>
-                                </a>
+                                <?if (($USER->IsAuthorized() && SITE_ID == "s2") || ($USER->IsAuthorized() && SITE_ID == "s1")) {?>
+                                    <a rel="nofollow" title="Авторизация для партнеров" class="personal-link dark-color" href="http://b2b.td-irbis.ru/personal/"><i class="svg inline  svg-inline-cabinet" aria-hidden="true" title="Мой кабинет"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="17" viewBox="0 0 14 17">
+                                                <defs>
+                                                    <style>
+                                                        .uscls-1 {
+                                                            fill: #222;
+                                                            fill-rule: evenodd;
+                                                        }
+                                                    </style>
+                                                </defs>
+                                                <path class="uscls-1" d="M13.969,16a1,1,0,1,1-2,0H11.927C11.578,14.307,9.518,13,7,13s-4.575,1.3-4.924,3H2.031a1,1,0,0,1-2,0,0.983,0.983,0,0,1,.1-0.424C0.7,12.984,3.54,11,7,11S13.332,13,13.882,15.6a1.023,1.023,0,0,1,.038.158c0.014,0.082.048,0.159,0.058,0.243H13.969ZM7,10a5,5,0,1,1,5-5A5,5,0,0,1,7,10ZM7,2a3,3,0,1,0,3,3A3,3,0,0,0,7,2Z"></path>
+                                            </svg>
+                                        </i>
+                                    </a>
+                                <?} else {?>
+                                    <a rel="nofollow" title="Авторизация для партнеров" class="personal-link dark-color" href="http://b2b.td-irbis.ru/auth/" target="_blank"><i class="svg inline  svg-inline-cabinet" aria-hidden="true" title="Мой кабинет"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="17" viewBox="0 0 14 17">
+                                                <defs>
+                                                    <style>
+                                                        .uscls-1 {
+                                                            fill: #222;
+                                                            fill-rule: evenodd;
+                                                        }
+                                                    </style>
+                                                </defs>
+                                                <path class="uscls-1" d="M13.969,16a1,1,0,1,1-2,0H11.927C11.578,14.307,9.518,13,7,13s-4.575,1.3-4.924,3H2.031a1,1,0,0,1-2,0,0.983,0.983,0,0,1,.1-0.424C0.7,12.984,3.54,11,7,11S13.332,13,13.882,15.6a1.023,1.023,0,0,1,.038.158c0.014,0.082.048,0.159,0.058,0.243H13.969ZM7,10a5,5,0,1,1,5-5A5,5,0,0,1,7,10ZM7,2a3,3,0,1,0,3,3A3,3,0,0,0,7,2Z"></path>
+                                            </svg>
+                                        </i>
+                                    </a>
+                                <?}?>
+
 							</div>
 						</div>
 					</div>
